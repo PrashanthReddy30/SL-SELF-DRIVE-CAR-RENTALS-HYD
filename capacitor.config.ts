80a70682f26292050791a51baec5da85cc8fa6e9
Your Name <your-email@example.com>
2026-09-26 17:08:28 +0530

    Add Capacitor iOS and Android frameworks

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.slrentals.app',
+  appName: 'SL Rentals',
+  webDir: 'dist'
+};
+
+export default config;
